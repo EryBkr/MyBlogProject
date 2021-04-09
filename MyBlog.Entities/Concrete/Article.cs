9d20388caf86ad031022c8ac2417153d273532eb
Eray Bakır <info@eraybakir.com>
2021-04-09 20:43:08 +0300

    Entites Create

---
+++ MyBlog.Entities/Concrete/Article.cs
@@ -0,0 +1,33 @@
+﻿using MyBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Entities.Concrete
+{
+    public class Article:EntityBase,IEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentsCount { get; set; }
+        public string SeoAuthor { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTags { get; set; }
+
+        //Navigation Property and FK
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+        //Navigation Property and FK
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ MyBlog.Entities/Concrete/User.cs
@@ -0,0 +1,28 @@
+﻿using MyBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Entities.Concrete
+{
+    public class User : EntityBase, IEntity
+    {
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string Username { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+
+        //Navigation Property and FK
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+
+       
+    }
+}
