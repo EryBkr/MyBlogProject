020479597d992f07daaae822c3b0db89fa897e73
Eray Bakır <info@eraybakir.com>
2021-04-10 13:52:28 +0300

    Result Model Created

---
+++ MyBlog.Shared/Utilities/Results/Abtracts/IResult.cs
@@ -0,0 +1,18 @@
+﻿using MyBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Shared.Utilities.Results.Abtracts
+{
+    public interface IResult
+    {
+        //Sadece Yapıcı Metotta değer ataması olması için set; kısımlarını kaldırdık
+        //Diğer türlü gelen datalar manipüle edilebilir
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
